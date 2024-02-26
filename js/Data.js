0236da169eada76530754298a780e6e621ec9112
Hasibul-Islam <hmhasibul48@gmail.com>
2024-02-26 10:37:54 +0600

    initial commit

---
+++ js/Data.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+        number: 1,
+        question: 'What is the main objective of the "Granny" game?',
+        answar: "Escape from Granny's house",
+        options: [
+            "Cook a delicious meal",
+            "Escape from Granny's house",
+            "Solve puzzles to rescue a pet",
+            "Decorate Granny's house"
+        ]
+    },
+
+    {
+        number: 2,
+        question: 'How many days do you typically have to survive in the game?',
+        answar: "5 days",
+        options: [
+            "1 day",
+            "5 days",
+            "7 days",
+            "10 days"
+        ]
+    },
+    {
+        number: 3,
+        question: 'What are some common items you can use to defend yourself in "Granny"?',
+        answar: "A tranquilizer dart and a hammer",
+        options: [
+            "A crossbow and a slingshot",
+            "A shotgun and a chainsaw",
+            " A tennis racket and a baseball bat",
+            "A tranquilizer dart and a hammer"
+        ]
+    },
+    {
+        number: 4,
+        question: 'Who is the main antagonist in the game "Granny"?',
+        answar: "Granny",
+        options: [
+            " A ghost",
+            "Granny",
+            " Grandpa",
+            "The Neighbor"
+        ]
+    },
+    {
+        number: 5,
+        question: 'What happens if Granny catches the player in the game?',
+        answar: "You lose a day",
+        options: [
+            "You get a new item",
+            "Granny bakes you cookies",
+            "You lose a day",
+            "You win the game"
+        ]
+    }
+    // {
+    //     number: 6,
+    //     question: "How can you escape Granny's house in the game?",
+    //     answar: "Repair the car",
+    //     options: [
+    //         "Find a hidden key",
+    //         "Repair the car",
+    //         "Unlock the plane",
+    //         "Wait for Granny to fall a sleep"
+    //     ]
+    // },
+    // {
+    //     number: 7,
+    //     question: 'What is the significance of the teddy bear in "Granny"?',
+    //     answar: "It helps distract Granny",
+    //     options: [
+    //         "It's a weapon to fight Granny",
+    //         "It's a collectible item",
+    //         "It helps distract Granny",
+    //         "It has a secret code hidden inside"
+    //     ]
+    // },
+    // {
+    //     number: 8,
+    //     question: 'Which difficulty level is the most challenging in "Granny"?',
+    //     answar: "Nightmare",
+    //     options: [
+    //         "Easy",
+    //         "Normal",
+    //         "Nightmare",
+    //         "Hard"
+    //     ]
+    // },
+    // {
+    //     number: 9,
+    //     question: 'What is the name of the main character you control in "Granny"?',
+    //     answar: "The Player",
+    //     options: [
+    //         "Jack",
+    //         "Granny",
+    //         "Sarah",
+    //         "The Player"
+    //     ]
+    // },
+    // {
+    //     number: 10,
+    //     question: 'What is the primary tool used for unlocking doors in the "Granny" game?',
+    //     answar: "A lockpick",
+    //     options: [
+    //         "A crowbar",
+    //         "A master key",
+    //         "A screwdriver",
+    //         "A lockpick"
+    //     ]
+    // }
+
+];
+// MADE BY HASIBUL ISLAM
---
+++ js/script.js
@@ -0,0 +1,148 @@
+const Fbtn = document.querySelector(".btnWrapper button"), page2 = document.querySelector(".page2"), page3 = document.querySelector(".page3"), exitBtn = document.querySelector(".p3Btn1"), continuebtn = document.querySelector(".p3Btn2");
+
+Fbtn.addEventListener("click", function () {
+    page2.style.display = "block";
+})
+exitBtn.addEventListener("click", function () {
+    page2.style.display = "none";
+})
+
+continuebtn.addEventListener("click", function () {
+    page3.style.display = "block";
+    showDynamically(0);
+    setTimer(30)
+});
+
+let lastPage = document.querySelector('.page4');
+
+let queCount = 0;
+
+let crrentTime = document.querySelector("#crrTime");
+let counter;
+let nextBtn = document.querySelector('.qnB');
+
+
+let showResult = document.querySelector(".page4");
+
+nextBtn.addEventListener("click", function () {
+    if (queCount < questions.length - 1) {
+        queCount++;
+        showDynamically(queCount);
+        clearInterval(counter);
+        setTimer(30)
+        nextBtn.style.display = "none";
+    } else {
+        showResult.style.display = "block";
+    }
+})
+let optionsM = document.querySelector('.optionWrapper');
+
+function showDynamically(indexNumber) {
+    const maniQuestion = document.querySelector(".crrQuestion");
+    maniQuestion.innerHTML = questions[indexNumber].number + "." + " " + questions[indexNumber].question;
+
+    // let optionsM = document.querySelector('.optionWrapper');
+    optionsM.innerHTML = `<li  class="options">` + questions[indexNumber].options[0] + `</li>` + `<li  class="options">` + questions[indexNumber].options[1] + `</li>` +
+        `<li  class="options">` + questions[indexNumber].options[2] + `</li>` +
+        `<li  class="options">` + questions[indexNumber].options[3] + `</li>`;
+
+    document.querySelector(".qnP").innerHTML = `${indexNumber + 1} of ${questions.length} Questions`;
+
+    const option = optionsM.querySelectorAll(".options");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+
+}
+let points = 0;
+let WrongPoints = 0;
+let scoreArea = document.querySelector(".scoreArea");
+const tikIcon = `<div class="tikIcon"><i class="fa fa-check" aria-hidden="true"></i></div>`, crossIcon = `<div class="crossIcon"><i class="fa fa-times" aria-hidden="true"></i></div>`;
+
+function optionSelected(answar) {
+    let userAnswar = answar.textContent;
+    let rightAnswar = questions[queCount].answar;
+    let alloptions = optionsM.children.length;
+    clearInterval(counter);
+
+    if (userAnswar === rightAnswar) {
+        answar.classList.add("currect");
+        // console.log("currect");
+        answar.insertAdjacentHTML("beforeend", tikIcon);
+        points += 1;
+        console.log(points);
+        if (points === 0) {
+            points = 0;
+        }
+    } else {
+        answar.classList.add("incurrect");
+        // console.log("Incurrect");
+        answar.insertAdjacentHTML("beforeend", crossIcon)
+        for (i = 0; i < alloptions; i++) {
+            if (optionsM.children[i].textContent === rightAnswar) {
+                optionsM.children[i].classList.add("options");
+                optionsM.children[i].classList.add("currect");
+                optionsM.children[i].insertAdjacentHTML("beforeend", tikIcon);
+            }
+        }
+        WrongPoints += 1;
+        console.log(WrongPoints);
+        if (WrongPoints === 0) {
+            WrongPoints = 0;
+        }
+    }
+    dynamicResult(points, WrongPoints);
+
+    for (let i = 0; i < alloptions; i++) {
+        optionsM.children[i].classList.add("disabled");
+    }
+    nextBtn.style.display = "block";
+}
+
+function setTimer(time) {
+    counter = setInterval(timerCount, 1000);
+    let alloptions = optionsM.children.length;
+    let rightAnswar = questions[queCount].answar;
+    function timerCount() {
+        crrentTime.textContent = time;
+
+        time--
+
+        if (time <= 9) {
+            crrentTime.textContent = "0" + time;
+        }
+        if (time <= 0) {
+            clearInterval(counter);
+        }
+        if (time === 0) {
+            for (let i = 0; i < alloptions; i++) {
+                optionsM.children[i].classList.add("disabled");
+            }
+
+            nextBtn.style.display = "block";
+
+            for (i = 0; i < alloptions; i++) {
+                if (optionsM.children[i].textContent === rightAnswar) {
+                    optionsM.children[i].classList.add("options");
+                    optionsM.children[i].classList.add("currect");
+                    optionsM.children[i].insertAdjacentHTML("beforeend", tikIcon);
+                }
+            }
+        }
+    }
+
+}
+
+function dynamicResult(points, WrongPoints) {
+    scoreArea.innerHTML = `<h1 style= "margin-bottom: 20px; text-align: center">Your Score</h1><p>Currect Answar(s) : ${points}</p><p>Wrong Answar(s) : ${WrongPoints}</p><p>Missed: ${questions.length - (points + WrongPoints)}</p><p>Total mark(s): ${points} Out Of ${questions.length}</p>`
+}
+
+document.querySelector(".exitQBtn").addEventListener("click", function () {
+    window.location.reload();
+});
+
+document.querySelector(".replayBtn").addEventListener("click", function () {
+    window.location.reload();
+})
+
+// MADE BY HASIBUL ISLAM
